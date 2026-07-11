329e47771613a1ccac57211cede4d4564a793ed4
ViperClaw1 <bax.nick93@gmail.com>
2026-07-11 13:13:58 +0400

    update dockerfile - fix docker compose parallel building api and worker images

--- apps/worker/dist/app.module.js
+++ apps/worker/dist/app.module.js
@@ -9,42 +9,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
-const browser_service_1 = require("./browser/browser.service");
-const open_form_step_1 = require("./steps/open-form.step");
-const validate_schema_step_1 = require("./steps/validate-schema.step");
-const fill_fields_step_1 = require("./steps/fill-fields.step");
-const attach_files_step_1 = require("./steps/attach-files.step");
-const submit_form_step_1 = require("./steps/submit-form.step");
-const log_result_step_1 = require("./steps/log-result.step");
-const field_mapper_1 = require("./filler/field.mapper");
-const file_attacher_1 = require("./filler/file.attacher");
-const form_filler_1 = require("./filler/form.filler");
-const submit_handler_1 = require("./filler/submit.handler");
-const notifications_service_1 = require("./notifications/notifications.service");
-const processor_1 = require("./processor");
-const prisma_module_1 = require("./prisma/prisma.module");
-const screenshot_service_1 = require("./screenshot/screenshot.service");
+const browser_service_js_1 = require("./browser/browser.service.js");
+const open_form_step_js_1 = require("./steps/open-form.step.js");
+const validate_schema_step_js_1 = require("./steps/validate-schema.step.js");
+const fill_fields_step_js_1 = require("./steps/fill-fields.step.js");
+const attach_files_step_js_1 = require("./steps/attach-files.step.js");
+const submit_form_step_js_1 = require("./steps/submit-form.step.js");
+const log_result_step_js_1 = require("./steps/log-result.step.js");
+const field_mapper_js_1 = require("./filler/field.mapper.js");
+const file_attacher_js_1 = require("./filler/file.attacher.js");
+const form_filler_js_1 = require("./filler/form.filler.js");
+const submit_handler_js_1 = require("./filler/submit.handler.js");
+const notifications_service_js_1 = require("./notifications/notifications.service.js");
+const processor_js_1 = require("./processor.js");
+const prisma_module_js_1 = require("./prisma/prisma.module.js");
+const screenshot_service_js_1 = require("./screenshot/screenshot.service.js");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [config_1.ConfigModule.forRoot({ isGlobal: true }), prisma_module_1.PrismaModule],
+        imports: [config_1.ConfigModule.forRoot({ isGlobal: true }), prisma_module_js_1.PrismaModule],
         providers: [
-            browser_service_1.BrowserService,
-            screenshot_service_1.ScreenshotService,
-            field_mapper_1.FieldMapper,
-            file_attacher_1.FileAttacher,
-            form_filler_1.FormFiller,
-            submit_handler_1.SubmitHandler,
-            open_form_step_1.OpenFormStep,
-            validate_schema_step_1.ValidateSchemaStep,
-            fill_fields_step_1.FillFieldsStep,
-            attach_files_step_1.AttachFilesStep,
-            submit_form_step_1.SubmitFormStep,
-            log_result_step_1.LogResultStep,
-            notifications_service_1.NotificationsService,
-            processor_1.Processor,
+            browser_service_js_1.BrowserService,
+            screenshot_service_js_1.ScreenshotService,
+            field_mapper_js_1.FieldMapper,
+            file_attacher_js_1.FileAttacher,
+            form_filler_js_1.FormFiller,
+            submit_handler_js_1.SubmitHandler,
+            open_form_step_js_1.OpenFormStep,
+            validate_schema_step_js_1.ValidateSchemaStep,
+            fill_fields_step_js_1.FillFieldsStep,
+            attach_files_step_js_1.AttachFilesStep,
+            submit_form_step_js_1.SubmitFormStep,
+            log_result_step_js_1.LogResultStep,
+            notifications_service_js_1.NotificationsService,
+            processor_js_1.Processor,
         ],
     })
 ], AppModule);
--- apps/worker/src/app.module.ts
+++ apps/worker/src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { BrowserService } from './browser/browser.service';
-import { OpenFormStep } from './steps/open-form.step';
-import { ValidateSchemaStep } from './steps/validate-schema.step';
-import { FillFieldsStep } from './steps/fill-fields.step';
-import { AttachFilesStep } from './steps/attach-files.step';
-import { SubmitFormStep } from './steps/submit-form.step';
-import { LogResultStep } from './steps/log-result.step';
-import { FieldMapper } from './filler/field.mapper';
-import { FileAttacher } from './filler/file.attacher';
-import { FormFiller } from './filler/form.filler';
-import { SubmitHandler } from './filler/submit.handler';
-import { NotificationsService } from './notifications/notifications.service';
-import { Processor } from './processor';
-import { PrismaModule } from './prisma/prisma.module';
-import { ScreenshotService } from './screenshot/screenshot.service';
+import { BrowserService } from './browser/browser.service.js';
+import { OpenFormStep } from './steps/open-form.step.js';
+import { ValidateSchemaStep } from './steps/validate-schema.step.js';
+import { FillFieldsStep } from './steps/fill-fields.step.js';
+import { AttachFilesStep } from './steps/attach-files.step.js';
+import { SubmitFormStep } from './steps/submit-form.step.js';
+import { LogResultStep } from './steps/log-result.step.js';
+import { FieldMapper } from './filler/field.mapper.js';
+import { FileAttacher } from './filler/file.attacher.js';
+import { FormFiller } from './filler/form.filler.js';
+import { SubmitHandler } from './filler/submit.handler.js';
+import { NotificationsService } from './notifications/notifications.service.js';
+import { Processor } from './processor.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { ScreenshotService } from './screenshot/screenshot.service.js';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
